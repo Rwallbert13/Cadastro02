4776d9109c604481cb0a9efdf378c6152b4833f9
Renato Walbert Bastos de Menezes <rwallbert@hotmail.com>
2023-06-13 19:59:25 -0300

    Inicial

---
+++ src/main/java/com/mycompany/cadastro02/Cadastro02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastro02;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cadastro02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
